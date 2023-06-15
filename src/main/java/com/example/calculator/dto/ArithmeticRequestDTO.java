9ece4025498f3999c4c449f456b1ff0c8bb082dd
mdnehalsalik <msalik@mtu.edu>
2023-06-14 22:41:55 -0400

    Dtos cleaned and now works in spring inbuilt jakson parsing instead of builder pattern parser. [Working]

--- src/main/java/com/example/calculator/dto/ArithmeticRequestDTO.java
+++ src/main/java/com/example/calculator/dto/ArithmeticRequestDTO.java
@@ -1,6 +1,8 @@
 package com.example.calculator.dto;
 
 import com.example.calculator.utils.LoggerUtil;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.slf4j.Logger;
 
 import javax.validation.constraints.NotEmpty;
@@ -10,37 +12,42 @@ import java.util.List;
 public class ArithmeticRequestDTO {
 
     @NotNull(message = "Operation cannot be null")
-    private String operation;
+    private final String operation;
 
     @NotEmpty(message = "Values array cannot be empty")
-    private List<@NotNull(message = "Values array cannot contain null elements") Double> values;
+    private final List<@NotNull(message = "Values array cannot contain null elements") Double> values;
 
     private static final Logger logger = LoggerUtil.getLogger();
+
+    @JsonCreator
+    public ArithmeticRequestDTO(@JsonProperty("operation") String operation,
+                                @JsonProperty("values") List<Double> values) {
+        if (operation == null) {
+            logger.error("Operation cannot be null");
+            throw new IllegalArgumentException("Operation cannot be null");
+        }
+
+        if (values == null || values.isEmpty()) {
+            logger.error("Values array cannot be empty");
+            throw new IllegalArgumentException("Values array cannot be empty");
+        }
+
+        for (Double value : values) {
+            if (value == null) {
+                logger.error("Values array cannot contain null elements");
+                throw new IllegalArgumentException("Values array cannot contain null elements");
+            }
+        }
+
+        this.operation = operation;
+        this.values = values;
+    }
 
     public String getOperation() {
         return operation;
     }
 
-    public void setOperation(String operation) {
-        if (operation == null) {
-            logger.error("Operation cannot be null");
-        }
-        this.operation = operation;
-    }
-
     public List<Double> getValues() {
         return values;
-    }
-
-    public void setValues(List<Double> values) {
-        if (values == null || values.isEmpty()) {
-            logger.error("Values array cannot be empty");
-        }
-        for (Double value : values) {
-            if (value == null) {
-                logger.error("Values array cannot contain null elements");
-            }
-        }
-        this.values = values;
     }
 }
--- src/main/java/com/example/calculator/service/AccountsService.java
+++ src/main/java/com/example/calculator/service/AccountsService.java
@@ -1,7 +1,6 @@
 package com.example.calculator.service;
 
 import com.example.calculator.dto.AccountsRequestDTO;
-import com.example.calculator.dto.ArithmeticRequestDTO;
 import com.example.calculator.factory.AccountsFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
